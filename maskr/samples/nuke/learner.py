371477326efa919b4aa53deaf82cd3519cf4e549
simon <simonm3@gmail.com>
2018-11-28 20:14:42 +0000

    .

--- maskr/samples/nuke/learner.py
+++ maskr/samples/nuke/learner.py
@@ -3,7 +3,7 @@ import torch
 from fastai import *
 from maskr.models.maskrcnn import MaskRCNN
 from maskr.callbacks import *
-from .dataset import Dataset
+from maskr.samples.nuke.dataset import Dataset
 
 ROOT_DIR = "/home/ubuntu/maskr"
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
@@ -23,7 +23,7 @@ def get_data(config):
     # create validation sample
     pvalid = .2
     trainpath = join(DATA, "stage1_train")
-    df = pd.DataFrame(os.listdir(trainpath)[:9], columns=["image"])
+    df = pd.DataFrame(os.listdir(trainpath), columns=["image"])
     train = np.random.random(len(df))>pvalid
     df.loc[train, "subset"] = "train"
     df.loc[~train, "subset"] = "valid"
@@ -84,7 +84,6 @@ def get_model(config):
         {'params': trainables_wo_bn, 'weight_decay': model.config.WEIGHT_DECAY},
         {'params': trainables_only_bn}
     ], lr=learning_rate, momentum=model.config.LEARNING_MOMENTUM)
-
     return model
 
 def get_learn(config):
